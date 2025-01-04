90799280878a6a61d2aeac9cff180a88f6626d9e
Teja99860 <teja.m@ifocussystec.in>
2025-01-04 11:31:34 +0530

    first coommit

---
+++ src/components/Accordion.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Accordion = () => {
+  return <div className="flex items-center justify-center">Accordion</div>;
+};
+
+export default Accordion;
---
+++ src/components/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: "01",
+    question: "What is your favorite programming language?",
+    answer: "My favorite programming language is JavaScript.",
+  },
+  {
+    id: "02",
+    question: "What is your primary development environment?",
+    answer: "I primarily use Visual Studio Code for development.",
+  },
+  {
+    id: "03",
+    question: "What is your favorite JavaScript framework?",
+    answer: "My favorite JavaScript framework is React.",
+  },
+  {
+    id: "04",
+    question: "What version control system do you use?",
+    answer: "I use Git for version control.",
+  },
+];
